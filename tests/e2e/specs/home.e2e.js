d4a425a5962d57f342b2f806bbf1a8e061899fd1
andreydos <andrii3110@gmail.com>
2018-07-13 14:50:18 +0300

    test components, circle deploy update 3

--- tests/e2e/specs/home.e2e.js
+++ tests/e2e/specs/home.e2e.js
@@ -1,7 +1,7 @@
 describe('Home Page', () => {
   it('has the correct title and heading', () => {
     cy.visit('/')
-    cy.title().should('equal', 'Home | Vue GraphQL Enterprise Boilerplate')
+    cy.title().should('equal', 'Home | Fishing Portal')
     cy.contains('h1', 'Home Page')
   })
 })
